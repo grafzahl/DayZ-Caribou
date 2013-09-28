7cc0a10fcb4382446bc476c67717bd7522616391
Christian Vogt <c.vogt@xulu.net>
2013-09-28 02:30:35 +0200

    Initial Commit
    DayZ-Core 1.8

---
+++ SQF/dayz_code/Configs/CfgMagazines/Caribou.hpp
@@ -0,0 +1,138 @@
+//Bloodbag
+class ItemBloodbag: CA_Magazine {
+	scope=2;
+	count=1;
+	type=256;
+	displayName="$STR_EQUIP_NAME_16";
+	model="\dayz_equip\models\bloodbag.p3d";
+	picture="\dayz_equip\textures\equip_bloodbag_ca.paa";
+	descriptionShort="$STR_EQUIP_DESC_16";
+	class ItemActions {
+		class Use
+		{
+			text="Self-Bloodbag";
+			script="spawn player_selfBloodBag;";
+		};
+	};
+};
+
+//Skins
+class Skin_Camo2_DZ : SkinBase {
+	usemodel = "Camo2_DZ";
+	displayName = "Takistan Bandit Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+
+class Skin_Rebel1_DZ : SkinBase {
+	usemodel = "Rebel1_DZ";
+	displayName = "Rebel Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Rebel2_DZ : SkinBase {
+	usemodel = "Rebel2_DZ";
+	displayName = "Rebel Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Rebel3_DZ : SkinBase {
+	usemodel = "Rebel3_DZ";
+	displayName = "Rebel Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Rebel4_DZ : SkinBase {
+	usemodel = "Rebel4_DZ";
+	displayName = "Rebel Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Rebel5_DZ : SkinBase {
+	usemodel = "Rebel5_DZ";
+	displayName = "Rebel Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Rebel6_DZ : SkinBase {
+	usemodel = "Rebel6_DZ";
+	displayName = "Rebel Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+
+
+
+class Skin_Diver1_DZ : SkinBase {
+	usemodel = "Diver1_DZ";
+	displayName = "Diving-Suit";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Diver2_DZ : SkinBase {
+	usemodel = "Diver2_DZ";
+	displayName = "Neoprene-Suit";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+
+
+
+class Skin_Merc1_DZ : SkinBase {
+	usemodel = "Merc1_DZ";
+	displayName = "Takistan Bandit Skin";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Merc2_DZ : SkinBase {
+	usemodel = "Merc2_DZ";
+	displayName = "Mercenary Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Merc3_DZ : SkinBase {
+	usemodel = "Merc3_DZ";
+	displayName = "Scout Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Merc4_DZ : SkinBase {
+	usemodel = "Merc4_DZ";
+	displayName = "Mercenary Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Merc5_DZ : SkinBase {
+	usemodel = "Merc5_DZ";
+	displayName = "Mercenary Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+
+
+
+class Skin_BlackOps1_DZ : SkinBase {
+	usemodel = "BlackOps1_DZ";
+	displayName = "BlackOps Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_BlackOps2_DZ : SkinBase {
+	usemodel = "BlackOps2_DZ";
+	displayName = "BlackOps Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_BlackOps3_DZ : SkinBase {
+	usemodel = "BlackOps3_DZ";
+	displayName = "BlackOps Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_BlackOps4_DZ : SkinBase {
+	usemodel = "BlackOps4_DZ";
+	displayName = "BlackOps Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_BlackOps5_DZ : SkinBase {
+	usemodel = "BlackOps5_DZ";
+	displayName = "BlackOps Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+
+
+
+
+class Skin_Pilot1_DZ : SkinBase {
+	usemodel = "Pilot1_DZ";
+	displayName = "Pilot Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
+class Skin_Pilot2_DZ : SkinBase {
+	usemodel = "Pilot2_DZ";
+	displayName = "Pilot Clothing";
+	descriptionShort = "Clothing-Parcel with a used Pre-Outbreak Suit";
+};
